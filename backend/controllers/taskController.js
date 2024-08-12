85c340b69250361463188e11f21c93b38de3ed7a
SebaViana <vianasebastian2000@gmail.com>
2024-08-12 23:12:13 +0000

    build express api with task routes and controllerrs

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,61 @@
+const { Task } = require('../models');
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.id);
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).send('Task not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.id);
+    if (task) {
+      await task.update(req.body);
+      res.json(task);
+    } else {
+      res.status(404).send('Task not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.id);
+    if (task) {
+      await task.destroy();
+      res.send('Task deleted');
+    } else {
+      res.status(404).send('Task not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
