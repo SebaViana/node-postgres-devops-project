1121f897ca7ea6bd8c91ed92c08245710a08ea1a
SebaViana <vianasebastian2000@gmail.com>
2024-08-14 13:39:15 +0000

    implemented env vars for postgres

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-if [ -z "$POSTGRES_DB" ]; then
-  echo "Error: POSTGRES_DB environment variable is required."
-  exit 1
-fi
+# Function to check if a required environment variable is set
+check_required_env_var() {
+  if [ -z "$1" ]; then
+    echo "Error: $2 environment variable is required."
+    exit 1
+  fi
+}
 
-if [ -z "$POSTGRES_USER" ]; then
-  echo "Error: POSTGRES_USER environment variable is required."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_PASSWORD" ]; then
-  echo "Error: POSTGRES_PASSWORD environment variable is required."
-  exit 1
-fi
-if [ -z "$POSTGRES_PORT" ]; then
-  echo "Error: POSTGRES_PORT environment variable is required."
-  exit 1
-fi
+# Check all required environment variables
+check_required_env_var "$POSTGRES_DB" "POSTGRES_DB"
+check_required_env_var "$POSTGRES_USER" "POSTGRES_USER"
+check_required_env_var "$POSTGRES_PASSWORD" "POSTGRES_PASSWORD"
+check_required_env_var "$POSTGRESS_INTERNAL_PORT" "POSTGRESS_INTERNAL_PORT"
 
 exec /usr/local/bin/docker-entrypoint.sh postgres
