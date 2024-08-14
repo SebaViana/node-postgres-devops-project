ee80d7c25a298c24cdb0b9636801a93e0073c74a
SebaViana <vianasebastian2000@gmail.com>
2024-08-14 13:01:46 +0000

    added env vars for backend

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/tasks', tasks);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const BACKEND_INTERNAL_PORT = process.env.BACKEND_INTERNAL_PORT;
+app.listen(BACKEND_INTERNAL_PORT, () => {
+  console.log(`Server is running on port ${BACKEND_INTERNAL_PORT}`);
 });
 
